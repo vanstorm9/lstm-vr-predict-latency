109ca8ad9f52015b4c8a31ad58902006a4fc0b53
vanstorm9 <antlowhur@yahoo.com>
2016-11-12 17:57:58 -0500

    Adding some stuff

--- model.py
+++ model.py
@@ -16,62 +16,77 @@ def create_dataset(dataset, look_back=1):
 		dataY.append(dataset[i + look_back, 0])
 	return numpy.array(dataX), numpy.array(dataY)
 
-# fix random seed for reproducibility
-numpy.random.seed(7)
+
+def trainModel(dataframe, dataset):
+	##### VALUES TO EDIT #####	
+	look_back = 3
+	#numInputNodes = 9
+	numInputNodes = 1
+	#########################
+
+	# fix random seed for reproducibility
+	numpy.random.seed(7)
+
+
+	# normalize the dataset
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	dataset = scaler.fit_transform(dataset)
+
+	# split into train and test sets
+	train_size = int(len(dataset) * 0.67)
+	test_size = len(dataset) - train_size
+	train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+
+	# reshape into X=t and Y=t+1
+	trainX, trainY = create_dataset(train, look_back)
+	testX, testY = create_dataset(test, look_back)
+
+	# reshape input to be [samples, time steps, features]
+	trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+	testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+
+
+
+	# create and fit the LSTM network
+	model = Sequential()
+	model.add(LSTM(4, input_dim=numInputNodes))
+	model.add(Dense(1))
+	model.compile(loss= 'mean_squared_error' , optimizer= 'adam' )
+	model.fit(trainX, trainY, nb_epoch=100, batch_size=1, verbose=2)
+
+	# Estimate model performance
+	trainScore = model.evaluate(trainX, trainY, verbose=0)
+	print( 'Train Score:' , scaler.inverse_transform(numpy.array([[trainScore]])))
+	testScore = model.evaluate(testX, testY, verbose=0)
+	print( 'Test Score:' , scaler.inverse_transform(numpy.array([[testScore]])))
+
+	# generate predictions for training
+	trainPredict = model.predict(trainX)
+	testPredict = model.predict(testX)
+
+	# shift train predictions for plotting
+	trainPredictPlot = numpy.empty_like(dataset)
+	trainPredictPlot[:, :] = numpy.nan
+	trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
+	# shift test predictions for plotting
+	testPredictPlot = numpy.empty_like(dataset)
+	testPredictPlot[:, :] = numpy.nan
+	testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+
+	# plot baseline and predictions
+	plt.plot(dataset)
+	plt.plot(trainPredictPlot)
+	plt.plot(testPredictPlot)
+	plt.show()
+
+	return model
+
 
 # load the dataset
-dataframe = pandas.read_csv( 'sample-scripts/data/international-airline-passengers.csv' , usecols=[1],
+dataframe = pandas.read_csv('sample-scripts/data/international-airline-passengers.csv' , usecols=[1],
 engine= 'python' , skipfooter=3)
 dataset = dataframe.values
 dataset = dataset.astype( 'float32')
 
-# normalize the dataset
-scaler = MinMaxScaler(feature_range=(0, 1))
-dataset = scaler.fit_transform(dataset)
-
-# split into train and test sets
-train_size = int(len(dataset) * 0.67)
-test_size = len(dataset) - train_size
-train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-
-# reshape into X=t and Y=t+1
-look_back = 3
-trainX, trainY = create_dataset(train, look_back)
-testX, testY = create_dataset(test, look_back)
-
-# reshape input to be [samples, time steps, features]
-trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
-testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
-
-# create and fit the LSTM network
-model = Sequential()
-model.add(LSTM(4, input_dim=1))
-model.add(Dense(1))
-model.compile(loss= 'mean_squared_error' , optimizer= 'adam' )
-model.fit(trainX, trainY, nb_epoch=100, batch_size=1, verbose=2)
-
-# Estimate model performance
-trainScore = model.evaluate(trainX, trainY, verbose=0)
-print( 'Train Score:' , scaler.inverse_transform(numpy.array([[trainScore]])))
-testScore = model.evaluate(testX, testY, verbose=0)
-print( 'Test Score:' , scaler.inverse_transform(numpy.array([[testScore]])))
-
-# generate predictions for training
-trainPredict = model.predict(trainX)
-testPredict = model.predict(testX)
-
-# shift train predictions for plotting
-trainPredictPlot = numpy.empty_like(dataset)
-trainPredictPlot[:, :] = numpy.nan
-trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-
-# shift test predictions for plotting
-testPredictPlot = numpy.empty_like(dataset)
-testPredictPlot[:, :] = numpy.nan
-testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-
-# plot baseline and predictions
-plt.plot(dataset)
-plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
-plt.show()
+trainModel(dataframe, dataset)
